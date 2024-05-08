6e5fb390285045e86649485e3effe8be18b5ffa5
S-A-oghene <agbamogunjacob@gmail.com>
2024-05-08 22:35:49 +0100

    week2-picture-album

---
+++ scripts/temples.js
@@ -0,0 +1,16 @@
+const hamburger = document.querySelector("#hamburger");
+const navItems = document.querySelector("#navItems");
+
+hamburger.addEventListener("click", () => {
+  navItems.classList.toggle("open");
+  hamburger.classList.toggle("open");
+});
+
+const today = new Date();
+const currentYear = document.querySelector("#currentyear");
+currentYear.textContent = today.getFullYear();
+
+const lastModified = document.querySelector("#lastModified");
+const dateModif = document.lastModified;
+
+lastModified.innerHTML = `Last Modification: ${dateModif}`;
